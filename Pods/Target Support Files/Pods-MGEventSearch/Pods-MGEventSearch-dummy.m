e18d7d437953e950fbf4824dcb2f7936e9c0e1ce
Mrigank Gupta <mrigankgupta@gmail.com>
2019-12-11 13:35:35 +0530

    add pod files

---
+++ Pods/Target Support Files/Pods-MGEventSearch/Pods-MGEventSearch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MGEventSearch : NSObject
+@end
+@implementation PodsDummy_Pods_MGEventSearch
+@end
---
+++ Pods/Target Support Files/Pods-MGEventSearchTests/Pods-MGEventSearchTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MGEventSearchTests : NSObject
+@end
+@implementation PodsDummy_Pods_MGEventSearchTests
+@end
